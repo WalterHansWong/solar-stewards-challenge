7797487e70d1f470b0a621ec99e80b8ac5acfbf4
Walter Wong <55761551+WalterHansWong@users.noreply.github.com>
2023-11-16 14:12:38 -0700

    Updated design of search results

--- pages/searchResults.js
+++ pages/searchResults.js
@@ -3,31 +3,65 @@ import { useRouter } from 'next/router';
 
 const SearchResults = () => {
   const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const { query } = router.query;
 
   useEffect(() => {
-    console.log("Search query received:", query);
     if (query) {
+      setIsLoading(true);
       fetch(`/api/search?term=${encodeURIComponent(query)}`)
         .then(res => res.json())
-        .then(data => setResults(data))
-        .catch(error => console.error('Error fetching data:', error));
+        .then(data => {
+          setResults(data);
+          setIsLoading(false);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+          setIsLoading(false);
+        });
     }
   }, [query]);
 
+  const handleSaveItem = (product) => {
+    // Implement logic to save the item to MongoDB
+    // Use an API request to server to handle this
+    console.log('Saving item:', product);
+  };
+
+  const navigateHome = () => {
+    router.push('/'); // Navigate to the home page
+  };
+
+  const navigateToSavedItems = () => {
+    router.push('/savedItems'); // TODO: Navigate to the saved items page
+  };
+
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
       <h1>Search Results</h1>
-      <ul>
-        {results.map(product => (
-          <li key={product.sku}>
-            {product.name} - ${product.salePrice}
-          </li>
-        ))}
-      </ul>
+      <div style={{ marginBottom: '20px' }}>
+        <button onClick={() => router.push('/')} style={{ marginRight: '10px' }}>New Search</button>
+        <button onClick={() => router.push('/savedItems')}>See Saved Items</button>
+      </div>
+      {isLoading ? (
+        <p>Fetching Results from BestBuy...</p>
+      ) : results.length > 0 ? (
+        <ul style={{ listStyle: 'none', padding: 0, width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          {results.map(product => (
+            <li key={product.sku} style={{ border: '1px solid #ddd', width: '80%', maxWidth: '500px', margin: '10px', padding: '10px', borderRadius: '8px', textAlign: 'left', wordWrap: 'break-word' }}>
+              <div>Name: {product.name}</div>
+              <div>Price: ${product.salePrice}</div>
+              <button onClick={() => handleSaveItem(product)} style={{ marginTop: '10px' }}>Save to Wishlist</button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No results found</p>
+      )}
     </div>
   );
 };
 
 export default SearchResults;
+
