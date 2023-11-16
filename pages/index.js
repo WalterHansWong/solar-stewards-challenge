82220864db82bb9c14e983f3c5c6f9080e9909ce
Walter Wong <55761551+WalterHansWong@users.noreply.github.com>
2023-11-16 13:38:02 -0700

    Updated design of home page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Router from 'next/router';
 
 const Index = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [results, setResults] = useState([]);
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
@@ -15,16 +14,28 @@ const Index = () => {
   };
 
   return (
-    <div>
-      <h1>Enter the product you are looking for:</h1>
-      <form onSubmit={handleSearchSubmit}>
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column'
+    }}>
+      <h1>Enter Keywords to Search for Products on BestBuy:</h1>
+      <form onSubmit={handleSearchSubmit} style={{ display: 'flex', alignItems: 'center' }}>
         <input 
           type="text" 
           placeholder="Search products..." 
           value={searchTerm} 
           onChange={handleSearchChange} 
+          style={{ 
+            flex: 1, 
+            minWidth: '300px', // Adjust the size as needed
+            padding: '10px', // Makes the input taller
+            marginRight: '10px' // Adds spacing between input and button
+          }} 
         />
-        <button type="submit">Search</button>
+        <button type="submit" style={{ padding: '10px 20px' }}>Search</button>
       </form>
     </div>
   );
